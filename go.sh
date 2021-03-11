74d31d86eac757e38ee1999baad0e2d82bba6c62
Nick Donnelly <nickd3000@hotmail.com>
2021-03-11 01:39:11 +0000

    Minor changes

--- go.sh
+++ go.sh
@@ -1,1 +1,2 @@
-java -jar ./target/jex-1.0-SNAPSHOT-jar-with-dependencies.jar ./zelda.txt
+#java -jar ./target/jex-1.0-SNAPSHOT-jar-with-dependencies.jar ./zelda.txt
+java -jar ./target/jex-1.0-SNAPSHOT-jar-with-dependencies.jar ./castle.txt
